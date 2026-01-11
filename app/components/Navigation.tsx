302e5855ea41b0dd70a56ccec951433b4121534b
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-01-11 19:50:11 -0300

    feat(navigation): add Home link to Navigation component

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,14 @@
+import { Home } from "lucide-react";
+
 export default function Navigation() {
     return (
         <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
             <ul className="flex gap-6 md:gap-10 text-white text-md   md:text-xl backdrop-filter backdrop-brightness-150 px-8 py-3 backdrop-blur-xl rounded-full shadow-lg border border-white/5">
+                <li className="hover:text-indigo-300 transition-colors">
+                    <a href="#top">
+                        <Home className="w-5 h-5 inline-block mr-1 mb-0.5" />
+                    </a>
+                </li>
                 <li className="hover:text-indigo-300 transition-colors"><a href="#about">About</a></li>
                 <li className="hover:text-indigo-300 transition-colors"><a href="#projects">Projects</a></li>
                 <li className="hover:text-indigo-300 transition-colors"><a href="#contact">Contact</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,14 @@ import LoadingScreen from './components/LoadingScreen';
 import ToolsSwiper from './components/ToolsSwiper';
 import Projects from './components/Projects';
 import Education from './components/Education';
-import Skills from './components/Skills';
 import Experience from './components/Experience';
-import ContactForm from './components/ContactForm';
 import ContactPage from './components/ContactPage';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 
 export default function Page() {
 	return (
-		<div className="bg-grain h-full relative bg-black overflow-hidden">
+		<div id="top" className="bg-grain h-full relative bg-black overflow-hidden">
 				<LoadingScreen />
 				<ShaderBG />
 				<div className="relative z-10 h-full flex flex-col">
