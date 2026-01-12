09afb7f5e043f0cb60331286f37440e0d33c3b0a
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-01-12 11:03:40 -0300

    feat(navigation): refine styling of Navigation component for improved visual consistency

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -3,8 +3,8 @@ import { Home } from "lucide-react";
 export default function Navigation() {
     return (
         <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
-            <ul className="flex gap-6 md:gap-10 text-white text-md md:text-xl backdrop-filter backdrop-brightness-150 px-10 py-3 backdrop-blur-xl rounded-full shadow-lg border border-white/5">
-                <li className="hover:text-indigo-300 transition-colors">
+            <ul className="flex gap-6 md:gap-10 text-white text-md md:text-xl backdrop-filter backdrop-brightness-100 bg-white/5 px-10 py-3 backdrop-blur-lg rounded-full shadow-lg border border-white/10">
+                <li className="hover:text-indigo-300 hover:transition-colors">
                     <a href="#top">
                         <Home className="w-5 h-5 inline-block mr-1 mb-0.5" />
                     </a>
